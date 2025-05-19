b4f639a1b4261018b87b2672f5b996e01e864f6b
Utkarsh Srivastava <utkarshzz007@gmail.com>
2025-05-19 19:38:52 +0530

    Update DelegateCalls.sol

--- DelegateCalls.sol
+++ DelegateCalls.sol
@@ -7,7 +7,7 @@ contract B {
     uint256 public num;
     address public sender;
     uint256 public value;
-  
+    
     function setVars(uint256 _num) public payable {
         num = _num;
         sender = msg.sender;
